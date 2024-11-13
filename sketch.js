46d644a678cb8fd03e3f520673896eb6c4405462
Brynn Lewin <bhl21@fsu.edu>
2024-11-13 10:21:43 -0500

    PUSH

--- sketch.js
+++ sketch.js
@@ -4,8 +4,20 @@ let start;
 let startB;
 let startBsprite;
 let street;
+let streetsprite;
 let star;
 let starsprite;
+let car;
+let couch;
+let couchsprite;
+let drink;
+let drinksprite;
+let key;
+let keysprite;
+let shadow;
+let shadowsprite;
+let carsprite;
+
 let stage = 0;
 let sit1;
 let sit2;
@@ -22,7 +34,7 @@ let night;
 let club;
 let murmer;
 let end;
-let car;
+let cars;
 let angry;
 let yelling;
 let grunt;
@@ -36,26 +48,37 @@ function preload() {
 	//images 
   start = loadImage('assets/start.png');
   startB = loadImage('assets/startb.png');
-  street = loadImage('assets/street.jpg');
+  street = loadImage('assets/street.png');
   star = loadImage('assets/star.png');
+  car = loadImage('assets/car.png');
+  couch = loadImage('assets/couch.png');
+  drink = loadImage('assets/drink.png');
+  key = loadImage('assets/key.png');
+  shadow = loadImage('assets/shadow.png');
   //sounds
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight); 
-  fullscreen(true);
-
+  createCanvas(600, 500); 
+  displayMode('centered', 'pixelated', 10);
+  //fullscreen(true);
+  
+  streetsprite = new Sprite();
+  streetsprite.diameter = 100;
+  streetsprite.x = 300;
+  streetsprite.y = 550;
+  streetsprite.image = street;
 
   startBsprite = new Sprite();
   startBsprite.diameter = 100;
-  startBsprite.x = 750;
-  startBsprite.y = 600;
+  startBsprite.x = 350;
+  startBsprite.y = 400;
   startBsprite.image = startB;
 
   starsprite = new Sprite();
-  starsprite.diameter = 100;
-  starsprite.x = width / 2;
-  starsprite.y = height / 2;
+  starsprite.diameter = 50;
+  starsprite.x = 300;
+  starsprite.y = 400;
   starsprite.image = star;
 
 
@@ -72,10 +95,11 @@ function draw() {
 
   switch (stage) {
     case 0:
-	   image(start, 400, 100);
+	   image(start, 30, 10, 500, 500);
 
 	   startBsprite.visible = true;
 	   starsprite.visible = false;
+	   streetsprite.visible = false;
 	   
 	   if (startBsprite.mouse.presses()) {
 		 print("Start button is pressed.");
@@ -84,30 +108,29 @@ function draw() {
       break;
       
     case 1:
-        image(street, 1, 10);
-    
-
+	
       startBsprite.visible = false;
       starsprite.visible = true;
+	  streetsprite.visible = true;
 
-	  starsprite.draw()
-	  if (keyIsDown(LEFT_ARROW)) {
-		starsprite.x -= 5; 
+	  streetsprite.draw(0,200);
+	  
+	  starsprite.draw(300,250,10,10,5,5);
+
+	  //streetsprite.draw();
+	  if (keyIsDown(UP_ARROW) && streetsprite.y < 660) {
+		streetsprite.y += 5; 
+		console.log(streetsprite.y)
 	  }
-	  if (keyIsDown(RIGHT_ARROW)) {
-		starsprite.x += 5; 
+	  if (keyIsDown(DOWN_ARROW) && streetsprite.y > 0) {
+		streetsprite.y -= 5; 
 	  }
-	  if (keyIsDown(UP_ARROW)) {
-		starsprite.y -= 5; 
-	  }
-	  if (keyIsDown(DOWN_ARROW)) {
-		starsprite.y += 5; 
-	  }
+	
       break;
       
     case 2:
       // Code for Stage 2
       break;
   }
-  camera.y = starsprite.y;
+ // camera.y = streetsprite.y;
 }
